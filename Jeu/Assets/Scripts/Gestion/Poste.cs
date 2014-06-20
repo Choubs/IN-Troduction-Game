add4bd9a73d7ec9d85833f4ad82e37169b0bd7f0
Guillaume <guillaume.choub@orange.fr>
2014-06-20 18:24:08 +0200

    Mise en place des lumières sur le niveau coop

---
+++ Jeu/Assets/Scripts/Gestion/Poste.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Poste {
+
+	public string name;
+	public string imagePath;
+	public string desc;
+	public int numero;
+
+}
+
---
+++ Jeu/Assets/Scripts/Gestion/PostesEnd.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostesEnd : MonoBehaviour {
+
+	public MainGame game;
+	public GameObject MenuToClose;
+	
+	
+	void OnClick(){
+		MenuToClose.SetActive(false);
+		game.SetPosteSelected();
+	}
+}
